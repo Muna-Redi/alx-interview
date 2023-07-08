d4c0d4515deab85458e4c7c86868165a89b113d9
Muna-Redi <francischym@gmail.com>
2023-07-08 04:16:01 +0100

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboses.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Can all Lockboxes be unlocked """
+
+
+def canUnlockAll(boxes):
+    """ returns true if all boxes can be unlocked 
+        key will have to be same number as a box to
+        unlock the box and box[0] is already unlocked
+    """
+    canUnlockAll = False
+    boxKeys = {0: True}
+    numberOfBoxes = len(boxes)
+    while(True):
+
+        numberOfKeys = len(boxKeys)
+
+        for i in range(len(boxes)):
+            if boxes[i] and boxKeys.get(i, False):
+                for j in boxes[i]:
+                    if j < numberOfBoxes:
+                        boxKeys[j] = True
+                    boxes[i] = None
+
+        if not(len(boxKeys) > numberOfKeys):
+            break
+
+    if numberOfKeys == len(boxes):
+        canUnlockAll = True
+
+    return canUnlockAll
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+# Solution to a Technical question on Lockboxes
+
