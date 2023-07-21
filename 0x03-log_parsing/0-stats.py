c1c52505b0f13341058554f34491fedba59b5595
Muna-Redi <francischym@gmail.com>
2023-07-21 03:53:51 +0100

    new solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
+
 """ log parse """
 
 import sys
 
-""" Globals """
-total_size = 0
 
-line_count = 0
+def print_stat(key, size):
+    """ Prints stats info """
+    print("File size: {:d}".format(size))
+    for i in sorted(key.keys()):
+        if key[i] != 0:
+            print("{}: {:d}".format(i, key[i]))
 
-stat_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
 
+stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+stat_count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
+        if stat_count != 0 and stat_count % 10 == 0:
+            print_stat(stat_codes, size)
 
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
+        stlist = line.split()
+        stat_count += 1
 
-            if status_code in stat_codes.keys():
-                stat_codes[status_code] += 1
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
 
-            total_size += file_size
+        try:
+            if stlist[-2] in stat_codes:
+                stat_codes[stlist[-2]] += 1
+        except Exception:
+            pass
+    print_stat(stat_codes, size)
 
-            line_count += 1
 
-        if line_count == 10:
-            line_count = 0
-            print('File size: {}'.format(total_size))
-
-            for key, value in sorted(stat_codes.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as e:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(stat_codes.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+except KeyboardInterrupt:
+    print_stat(stat_codes, size)
+    raise
